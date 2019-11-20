9f198822a793b00562132d8a81ea1944a35ba10b
Sofyen <soufyeinjamei1@gmail.com>
2019-11-20 16:45:42 +0100

    hello2

--- SpringTest/src/main/java/com/example/demo/controller/Controller.java
+++ SpringTest/src/main/java/com/example/demo/controller/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-    @RequestMapping("/helloWorldTest")
+    @RequestMapping("/helloWorldTest2")
     public String index() {
         return "Greetings from Spring Boot!";
     }
