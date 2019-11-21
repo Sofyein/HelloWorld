fb1509687d8afe840d2c129026be6becd9cf673c
Sofyein <31135813+Sofyein@users.noreply.github.com>
2019-11-21 09:57:38 +0100

    Update Controller.java

--- SpringTest/src/main/java/com/example/demo/controller/Controller.java
+++ SpringTest/src/main/java/com/example/demo/controller/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-    @RequestMapping("/helloWorldTest1")
+    @RequestMapping("/helloWorldTes1")
     public String index() {
         return "Greetings from Spring Boot!";
     }
